d3c4c4ef21e46bafb7635e43600220878f9a50de
timramone <timramone@gmail.com>
2014-11-08 22:56:06 +0300

    Initial template + WebRequestProxy

---
+++ mosdata/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using mosdata.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mosdata.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var result = WebRequestProxy.Instance.GetResponse("v1/datasets", "$skip=10&$top=5&$inlinecount=allpages");
+
+			return View((object)result);
+		}
+
+		public ActionResult Api(string path)
+		{
+			var result = WebRequestProxy.Instance.GetResponse(path, HttpContext.Request.QueryString.ToString());
+
+			return Content(result);
+		}
+	}
+}
---
+++ mosdata/Infrastructure/WebRequestProxy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace mosdata.Infrastructure
+{
+	public class WebRequestProxy
+	{
+		#region Singleton impl
+
+		public static readonly WebRequestProxy Instance;
+
+		static WebRequestProxy()
+		{
+			Instance = new WebRequestProxy();
+		}
+
+		#endregion
+
+		private const string baseUrlKey = "BaseApiUrl";
+		private readonly string baseUrl;
+
+		private WebRequestProxy()
+		{
+			baseUrl = System.Configuration.ConfigurationManager.AppSettings[baseUrlKey];
+		}
+		
+		public string GetResponse(string path, string query)
+		{
+			var uriBuilder = new UriBuilder();
+			uriBuilder.Host = baseUrl;
+			uriBuilder.Path = path;
+			uriBuilder.Query = HttpUtility.UrlDecode(query);
+
+			var client = new WebClient();
+			client.Encoding = Encoding.UTF8;
+
+			try
+			{
+				return client.DownloadString(uriBuilder.Uri);
+			}
+			catch (WebException ex)
+			{
+				throw new HttpException(404, "Что-то пошло не так");
+			}
+		}
+	}
+}
