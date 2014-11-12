fea1b4ca13e2bede718302418552e3783221b483
timramone <timramone@gmail.com>
2014-11-12 10:14:59 +0300

    nasty paging (thinking of viewmodels already)

--- mosdata/Scripts/JSX/Datasets.jsx
+++ mosdata/Scripts/JSX/Datasets.jsx
@@ -14,7 +14,9 @@ var DataSetDetailedInfo = React.createClass({
 
 var DataSet = React.createClass({
 	getInitialState: function () {
-		return {detailed: false};
+		return {
+			detailed: false
+		};
 	},
 	loadDataSetInfo: function(params) {
 		DataMosApi.loadDatasetInfo({
@@ -45,20 +47,44 @@ var DataSet = React.createClass({
 });	
 
 var DataSetsContainer = React.createClass({
-	loadDataSets: function(params) {
+	mergeDatasetsInfos: function(oldDatasetsInfo, newDatasetsInfo) {
+		var result = { 
+			Count: newDatasetsInfo.Count, 
+			Items: oldDatasetsInfo.Items.concat(newDatasetsInfo.Items)
+		};
+		return result;
+	},
+	loadDataSets: function() {
 		DataMosApi.loadDataSets({
-			skip: params.skip, 
-			top: params.top,
-			callback: function(datasetsInfo) {				
-				this.setState({initialized: true, datasetsInfo: datasetsInfo});
+			skip: this.state.currentPage * this.requestCount, 
+			top: this.requestCount,
+			callback: function(newDatasetsInfo) {
+				var resultDatasetsInfo = this.mergeDatasetsInfos(
+						this.state.datasetsInfo,
+						newDatasetsInfo)			
+				this.setState({
+					initialized: true, 
+					datasetsInfo: resultDatasetsInfo,
+					currentPage: this.state.currentPage + 1});
 			}.bind(this)
 		});
 	},
+	handleLoadMoreLinkClick: function(e) {
+		e.preventDefault();
+		this.loadDataSets();
+	},
 	getInitialState: function() {
-		return {initialized: false};
+		return {
+			initialized: false, 
+			currentPage: 0,
+			datasetsInfo: {
+				Items: [],
+				Count: 0
+			}};
 	},
 	componentWillMount: function() {
-		this.loadDataSets({skip: 0, top: 100});
+		this.requestCount = 100;
+		this.loadDataSets();
 	},
 	render: function () {
 		if (this.state.initialized){
@@ -73,14 +99,22 @@ var DataSetsContainer = React.createClass({
 							caption={datasetInfo.Caption} />);
 					});
 
+			var loadMoreBlock = (this.state.currentPage * this.requestCount) < this.state.datasetsInfo.Count ? 
+				(<a className="btn" href="#" onClick={this.handleLoadMoreLinkClick}>Загрузить ещё...</a>) : null;
+
 			return (
 				<div className="datasets-container">
-					<h3>Всего загружено наборов данных: {this.state.datasetsInfo.Count}</h3>
+					<h3>Всего наборов данных: {this.state.datasetsInfo.Count}</h3>
 					<div className="datasets container">
 						<div className="row">
 							{dataSetNodes}
 						</div>
 					</div>
+					<div className="container">
+						<div className="row">
+							{loadMoreBlock}
+						</div>
+					</div>					
 				</div>);	
 		} else {
 			return (<h3>Инициализация</h3>);	
